be45382b10cd28963e905efdc209122f54a823e3
Campanula <campanulamediuml@gmail.com>
2020-11-16 00:12:10 +0800

    打包

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 setup(
     # 以下为必需参数
     name='anduindata',  # 模块名
-    version='1.0.0',  # 当前版本
+    version='1.0.1',  # 当前版本
     description='a mysql connector',  # 简短描述
     py_modules=["anduindata"], # 单文件模块写法
     # ckages=find_packages(exclude=['contrib', 'docs', 'tests']),  # 多文件模块写法
@@ -25,14 +25,21 @@ setup(
     author_email='421248329@qq.com',
     platforms = 'any',
     keywords = "mysql",
-    packages = find_packages('absolute path or relative path of src'),
-    package_dir = {'':'absolute path or relative path of src'},
+    # packages = find_packages('anduin'),
+    # package_dir = {'anduin':'*'},
     classifiers = [
             'License :: OSI Approved :: MIT License',
             'Programming Language :: Python',
             'Intended Audience :: Developers',
             'Operating System :: OS Independent',
         ],
-    url = ''
+    url = 'https://github.com/campanulamediuml/Anduin',
+    install_requires=[
+          'pymysql==0.10.1',
+      ],
+    include_package_data=True,
+    zip_safe=True,
+    packages = ['anduin','anduin/dbserver'],
+    python_requires='>=3.2',
 
 )
